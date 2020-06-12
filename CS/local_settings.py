8c40a1650deee578a9b79732f2e2d44f439db4dd
Jaime Grimaldo <DankerGamer-@hotmail.com>
2020-06-12 14:18:10 -0500

    Comentario k

--- CS/local_settings.py
+++ CS/local_settings.py
@@ -9,5 +9,5 @@ SECRET_KEY = '^y#r=*2k-gm2_&3=8d*+==2g@y2v50nd%zpkj^zokl#_y*vnu*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
