2e959d448c43c0767862a5382fad1fe32cd27fdd
JaimeGrimaldo <46900357+JaimeGrimaldo@users.noreply.github.com>
2020-07-01 03:17:52 -0500

    Delete admin.py

--- Example1/admin.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.contrib import admin
-
-# Register your models here.
-=======
-from django.contrib import admin
-
-# Register your models here.
->>>>>>> f25c5189560cdafdaa701bf4640dde37531a69d4
