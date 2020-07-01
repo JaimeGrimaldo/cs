d2b2b7f08798f95649cd11198bd97a51824c14b5
Jaime Grimaldo <DankerGamer-@hotmail.com>
2020-07-01 00:28:32 -0500

    Modificacion de settings para ver si se refleja

--- CS/settings.py
+++ CS/settings.py
@@ -119,6 +119,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+# Ya no se que hacer para que funcione :D
 
 AUTH_PASSWORD_VALIDATORS = [
     {
